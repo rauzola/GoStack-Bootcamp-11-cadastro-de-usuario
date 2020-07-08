c80f21cd2e40b8d0b5a3832bfa6a9f9ec13303ce
rauzola <raul_sigoli@hotmail.com>
2020-07-08 17:37:11 -0300

    Relacionamento nos Models

--- src/database/migrations/1594230187231-CreateAppointments.ts
+++ src/database/migrations/1594230187231-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1594230187231
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
